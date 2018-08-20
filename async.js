589e9417aa67975bed71c2f52b97b70012d24ff5
Ashish kumar shrivastav <a.shrivastav@globallogic.com>
2018-08-20 22:26:08 +0530

    Async await vs promises

--- async.js
+++ async.js
@@ -15,4 +15,113 @@
 - Async function runs in the background
 - Ajax: Asynchronous javascript and xml. It allows us to communicate asynchronously with remote server.
 - Api : Application programming interface.
+
+Asnc Await:
+  - It is a new way to write asynchronous code. Previous options were callback and promises.
+  - Async/await is actually built on top of promises. It cannot be used with plain callbacks or
+    node callbacks.
+  - Async/await is like promises, non blocking.
+  - It makes asynchronous code look and behave like synchronous code. This is where all the power lies.
 */
+
+// Assuming a function getJSON that returns that promise and that promise resolves with some JSON object.
+// We want to call it and log that JSON and then return "done".
+// With plain promise
+function getJson() {
+  return fetch('https://jsonplaceholder.typicode.com/posts');
+}
+const makeRequest = () =>
+  getJson()
+    .then(data => {
+      console.log(data);
+      return "Done";
+    })
+
+makeRequest();
+
+// With async await
+const makeRequest2 = async () => {
+  console.log(await getJson());
+  return "Done";
+}
+
+// Our function has the keyword async before it. The await keyword can only be used inside the functions
+// defined with async. Any async function returns a promise implicitly and the resolved value of the promise
+// will be whatever you return from the function.
+// await getJson() means that console.log will wait until getJson() promise resolves and print its value.
+
+// Benefits of using Async/await
+// - Concise and clean
+// - Lesser code, no need to write .then
+// - Error handling : Async/await makes it finally possible to handle both async and sync errors with the
+//   same construct, good old try catch.
+// In the example below with promises, the try/catch will not handle if JSON.parse fails because
+// it’s happening inside a promise. We need to call .catch on the promise and duplicate our error
+// handling code, which will (hopefully) be more sophisticated than console.log
+// in your production ready code.
+const makeRequest3 = () => {
+  try {
+    getJson()
+      .then(result => {
+        console.log(result);
+        const data = JSON.parse(result.json());
+        console.log(data);
+      })
+      .catch(error => console.log(error));
+  }
+  catch (err){
+    console.log(err);
+  }
+}
+
+makeRequest3();
+
+// With async/await, catch block can also handles asynchronous errors
+const makeRequest4 = async () => {
+  try {
+    const data = JSON.parse(await getJson());
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+// Code below, when written using async/await becomes more clear and easy to understand
+const makeRequest5 = () => {
+  return getJSON()
+    .then(data => {
+      if (data.needsAnotherRequest) {
+        return makeAnotherRequest(data)
+          .then(moreData => {
+            console.log(moreData)
+            return moreData
+          })
+      } else {
+        console.log(data)
+        return data
+      }
+    })
+}
+
+// Using async/await
+const makeRequest6 = async () => {
+  const data = await getJSON()
+  if (data.needsAnotherRequest) {
+    const moreData = await makeAnotherRequest(data);
+    console.log(moreData)
+    return moreData
+  } else {
+    console.log(data)
+    return data
+  }
+}
+
+// When dealing with multiple promises
+const makeRequest7 = async () => {
+  const value1 = await promise1()
+  const value2 = await promise2(value1)
+  return promise3(value1, value2)
+}
+
+// Async await are too easy to debug.. They behaves just like synchronous code while debugging in the console.
+
